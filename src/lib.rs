d19a8f92d9524d705122fe5a45312d504c61d66d
Michael Miller <gammaphi42@gmail.com>
2022-07-01 10:52:23 -0600

    ran fmt

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,5 @@
-use std::error::Error;
 use clap::{App, Arg};
+use std::error::Error;
 use std::fs::File;
 use std::io::{self, BufRead, BufReader};
 
@@ -19,31 +19,29 @@ fn open(filename: &str) -> MyResult<Box<dyn BufRead>> {
     }
 }
 
-
-fn read_lines(file_handle: Box<dyn BufRead>, number_lines : bool, number_nonblank_lines : bool) {
+fn read_lines(file_handle: Box<dyn BufRead>, number_lines: bool, number_nonblank_lines: bool) {
     let mut line_number = 0;
     for line in file_handle.lines() {
-        
         let line_to_print = line.unwrap();
-        
-        if (number_nonblank_lines && !line_to_print.is_empty() ) || number_lines {
-            line_number += 1;    
+
+        if (number_nonblank_lines && !line_to_print.is_empty()) || number_lines {
+            line_number += 1;
             println!("{:>6}\t{}", line_number, line_to_print);
-        }
-        else {
+        } else {
             println!("{}", line_to_print);
         }
     }
-    
 }
 
-
 pub fn run(config: Config) -> MyResult<()> {
-    
     for filename in config.files {
         match open(&filename) {
             Err(err) => eprintln!("Failed to open {}: {}", filename, err),
-            Ok(_) => read_lines(open(&filename).unwrap(), config.number_lines, config.number_nonblank_lines),
+            Ok(_) => read_lines(
+                open(&filename).unwrap(),
+                config.number_lines,
+                config.number_nonblank_lines,
+            ),
         }
     }
     Ok(())
@@ -51,33 +49,32 @@ pub fn run(config: Config) -> MyResult<()> {
 
 pub fn get_args() -> MyResult<Config> {
     let matches = App::new("catr")
-    .version("0.1.0")
-    .author("Ken Youens-Clark <kyclark@gmail.com")
-    .about("Rust cat")
-    .arg(
-        Arg::with_name("files")
-        .value_name("FILES")
-        .help("Input files")
-        .multiple(true)
-        .min_values(1) // Is this needed with .default_value?
-        .default_value("-")
-    )
-    .arg(
-        Arg::with_name("number")
-        .short("n")
-        .long("number")
-        .help("Print line numbers")
-        .conflicts_with("number-nonblank")
-    )
-    .arg(
-        Arg::with_name("number-nonblank")
-        .short("b")
-        .long("number-nonblank")
-        .help("Print non-blank line numbers")
-        .conflicts_with("number")
-    )
-    .get_matches();
-
+        .version("0.1.0")
+        .author("Ken Youens-Clark <kyclark@gmail.com")
+        .about("Rust cat")
+        .arg(
+            Arg::with_name("files")
+                .value_name("FILES")
+                .help("Input files")
+                .multiple(true)
+                .min_values(1) // Is this needed with .default_value?
+                .default_value("-"),
+        )
+        .arg(
+            Arg::with_name("number")
+                .short("n")
+                .long("number")
+                .help("Print line numbers")
+                .conflicts_with("number-nonblank"),
+        )
+        .arg(
+            Arg::with_name("number-nonblank")
+                .short("b")
+                .long("number-nonblank")
+                .help("Print non-blank line numbers")
+                .conflicts_with("number"),
+        )
+        .get_matches();
 
     Ok(Config {
         files: matches.values_of_lossy("files").unwrap(),
